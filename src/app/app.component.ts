e5e2e2ab5e736cf6494af3cf00c2164e780a17e5
yarob-alrifai <yalrifai41@gmail.com>
2025-07-11 18:16:06 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import {Component} from '@angular/core';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+   imports: [CdkDropListGroup, CdkDropList, CdkDrag],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-drag-drop-todo';
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  wating :string[]= [];
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+
+    console.log(this.todo)
+  }
 }
